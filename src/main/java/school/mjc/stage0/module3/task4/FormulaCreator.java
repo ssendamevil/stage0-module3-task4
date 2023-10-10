fb8666f9e073c0e91c729fd879f1457b0c33a461
Ali Orynbasar <96694797+ssendamevil@users.noreply.github.com>
2023-10-10 20:58:59 +0600

    solved 1

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 1, b = 2;
+        double formula;
+
+        formula = (9*a*a  - 5*b + 2 +a - 7)/((a + b - 4*a*b)/2);
+
+        System.out.println(formula);
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.print(c);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(r);
     }
 }
